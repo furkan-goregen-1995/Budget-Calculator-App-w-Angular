b7fdbaf6088755e13d2bb1221fbf6b0b30e1bd5a
Furkan Göregen <goregenfurkan@gmail.com>
2021-01-14 17:21:08 +0300

    Eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    EditItemModalComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
+   entryComponents: [EditItemModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
